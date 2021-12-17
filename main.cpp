72846859eb6be075efcab2cd3c221929be2362c1
jenniferw2019 <jenniferwpdx2018@gmail.com>
2021-12-17 13:59:10 -0800

    update

--- main.cpp
+++ main.cpp
@@ -33,6 +33,23 @@ int main()
   char inputDescription[150];
 
   //create room and map of exits
+
+  Room* rmoutside;
+  strcpy(inputDescription, "outside");
+  rmoutside = new Room(inputDescription, outside);
+  map<char, char*> mpoutside;
+  mpoutside.insert(pair<char, char*>('s', onehall));
+  rmoutside->setExits(mpoutside);
+  vecRoomList.push_back(rmoutside);
+
+  Room* rmspanish;
+  strcpy(inputDescription, "in spanish");
+  rmspanish = new Room(inputDescription, spanish);
+  map<char, char*> mpspanish;
+  mpspanish.insert(pair<char, char*>('e', onehall));
+  rmspanish->setExits(mpspanish);
+  vecRoomList.push_back(rmspanish);
+  
   Room* rmonehall;
   strcpy(inputDescription, "in one hall");
   rmonehall = new Room(inputDescription, onehall);
@@ -55,6 +72,105 @@ int main()
   rmonetwenty->setExits(mponetwenty);
   vecRoomList.push_back(rmonetwenty);
 
+  Room* rmstorage;
+  strcpy(inputDescription, "in storage");
+  rmstorage = new Room(inputDescription, storage);
+  map<char, char*> mpstorage;
+  mpstorage.insert(pair<char, char*>('s', threehall));
+  mpstorage.insert(pair<char, char*>('w', onehall));
+  rmstorage->setExits(mpstorage);
+  vecRoomList.push_back(rmstorage);
+  
+  Room* rmfilm;
+  strcpy(inputDescription, "in film");
+  rmfilm = new Room(inputDescription, film);
+  map<char, char*> mpfilm;
+  mpfilm.insert(pair<char, char*>('e', onetwenty));
+  mpfilm.insert(pair<char, char*>('s', gym));
+  rmfilm->setExits(mpfilm);
+  vecRoomList.push_back(rmfilm);
+
+  Room* rmtheater;
+  strcpy(inputDescription, "in theater");
+  rmtheater = new Room(inputDescription, theater);
+  map<char, char*> mptheater;
+  mptheater.insert(pair<char, char*>('e', threehall));
+  mptheater.insert(pair<char, char*>('w', onetwenty));
+  rmtheater->setExits(mptheater);
+  vecRoomList.push_back(rmtheater);
+
+  Room* rmthreehall;
+  strcpy(inputDescription, "in three hall");
+  rmthreehall = new Room(inputDescription, threehall);
+  map<char, char*> mpthreehall;
+  mpthreehall.insert(pair<char, char*>('n', storage));
+  mpthreehall.insert(pair<char, char*>('e', library));
+  mpthreehall.insert(pair<char, char*>('s', cafe));
+  mpthreehall.insert(pair<char, char*>('w', theater));
+  rmthreehall->setExits(mpthreehall);
+  vecRoomList.push_back(rmthreehall);
+
+  Room* rmlibrary;
+  strcpy(inputDescription, "in library");
+  rmlibrary = new Room(inputDescription, library);
+  map<char, char*> mplibrary;
+  mplibrary.insert(pair<char, char*>('w', threehall));
+  rmlibrary->setExits(mplibrary);
+  vecRoomList.push_back(rmlibrary);
+
+  Room* rmlab;
+  strcpy(inputDescription, "in lab");
+  rmlab = new Room(inputDescription, lab);
+  map<char, char*> mplab;
+  mplab.insert(pair<char, char*>('n', onetwenty));
+  mplab.insert(pair<char, char*>('e', office));
+  mplab.insert(pair<char, char*>('s', twohall));
+  rmlab->setExits(mplab);
+  vecRoomList.push_back(rmlab);
+
+  Room* rmoffice;
+  strcpy(inputDescription, "in office");
+  rmoffice = new Room(inputDescription, office);
+  map<char, char*> mpoffice;
+  mpoffice.insert(pair<char, char*>('w', lab));
+  rmoffice->setExits(mplab);
+  vecRoomList.push_back(rmoffice);
+
+  Room* rmcafe;
+  strcpy(inputDescription, "in cafe");
+  rmcafe = new Room(inputDescription, cafe);
+  map<char, char*> mpcafe;
+  mpcafe.insert(pair<char, char*>('n', threehall));
+  rmcafe->setExits(mpcafe);
+  vecRoomList.push_back(rmcafe);
+
+  Room* rmgym;
+  strcpy(inputDescription, "in gym");
+  rmgym = new Room(inputDescription, gym);
+  map<char, char*> mpgym;
+  mpgym.insert(pair<char, char*>('n',film));
+  mpgym.insert(pair<char, char*>('e', twohall));
+  rmgym->setExits(mpgym);
+  vecRoomList.push_back(rmgym);
+
+  Room* rmtwohall;
+  strcpy(inputDescription, "in two hall");
+  rmtwohall = new Room(inputDescription, twohall);
+  map<char, char*> mptwohall;
+  mptwohall.insert(pair<char, char*>('n', lab));
+  mptwohall.insert(pair<char, char*>('e', art));
+  mptwohall.insert(pair<char, char*>('w', gym));
+  rmtwohall->setExits(mptwohall);
+  vecRoomList.push_back(rmtwohall);
+
+  Room* rmart;
+  strcpy(inputDescription, "in art");
+  rmart = new Room(inputDescription, art);
+  map<char, char*> mpart;
+  mpart.insert(pair<char, char*>('w', twohall));
+  rmart->setExits(mpart);
+  vecRoomList.push_back(rmart);
+  
   Room* currentRoom;
   map<char, char*> currentMap;
   currentRoom = rmonehall;
@@ -67,7 +183,20 @@ int main()
   strcpy(strtemp,currentMap.find('s')->second);
   cout << strtemp << endl;
 
-  
+  for(vector<Room*>::iterator it = vecRoomList.begin(); it!= vecRoomList.end(); it++)
+    {
+      if(strcmp((*it)->getRoomName(), strtemp) == 0)
+	{
+	  currentRoom = (*it);
+	  break;
+	}
+    }
+
+  currentMap = currentRoom->getExits();
+  cout << currentRoom->getRoomName() << endl;
+  cout << currentRoom->getRoomDescription() << endl;
+  strcpy(strtemp,currentMap.find('w')->second);
+  cout << strtemp << endl;
   
   /*
   vector<Item*> vecItemList;
